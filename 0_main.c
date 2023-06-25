6b6a261236afe1ee1180800a14a7064b7eaba10a
Renan Leite silva <rleite-s@c1r4p1.42sp.org.br>
2023-06-25 15:57:21 -0300

    fix: change set_shlvl function to list argument
    Before have leaks due envp recieve a mallocated string that not is freed in the main

--- 0_main.c
+++ 0_main.c
@@ -6,27 +6,27 @@
 /*   By:  rleite-s < rleite-s@student.42sp.org.b    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 15:10:13 by  rleite-s         #+#    #+#             */
-/*   Updated: 2023/06/24 15:22:54 by  rleite-s        ###   ########.fr       */
+/*   Updated: 2023/06/25 15:40:49 by  rleite-s        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	set_shlvl(char **envp)
+void	set_shlvl(t_env *env)
 {
 	int		level;
 	char	*char_level;
 	char	*join;
 
-	while (envp && *envp)
+	while (env)
 	{
-		if (strncmp(*envp, "SHLVL", 6) == '=')
+		if (ft_strncmp(env->var, "SHLVL", 6) == '=')
 			break ;
-		envp++;
+		env = env->next;
 	}
-	if (*envp)
+	if (env)
 	{
-		level = ft_atoi(*envp + 6);
+		level = ft_atoi(env->var + 6);
 		char_level = ft_itoa(level + 1);
 		if (!char_level)
 			return ;
@@ -34,7 +34,8 @@ void	set_shlvl(char **envp)
 		free(char_level);
 		if (!join)
 			return ;
-		*envp = join;
+		free(env->var);
+		env->var = join;
 	}
 }
 
@@ -48,13 +49,13 @@ int	main(int argc, char **argv, char **envp)
 		ft_putendl_fd("EXECUTE ME PROPERLY, YOU ASSHOLE!!! ┗|｀O′|┛", 2);
 		return (2);
 	}
-	set_shlvl(envp);
 	list = pointer_to_list(envp);
 	if (!list)
 	{
 		ft_putendl_fd("You don't have space, bro! (っ˘̩╭╮˘̩)っ", 2);
 		return (ENOMEM);
 	}
+	set_shlvl(list);
 	ret = minishell(&list);
 	free_list(&list);
 	rl_clear_history();
