6e81bfeb59c3d7cb6068efee547affdc3ccc0a84
Renan Leite silva <rleite-s@c1r4p1.42sp.org.br>
2023-06-25 16:00:16 -0300

    fix: validation of NULL pointer
    The adjustment is make before the check of the quotemarks. Thus, I can have a NULL result in the return of strchr

--- 5_normalize_input.c
+++ 5_normalize_input.c
@@ -6,7 +6,7 @@
 /*   By:  rleite-s < rleite-s@student.42sp.org.b    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 15:11:17 by  rleite-s         #+#    #+#             */
-/*   Updated: 2023/06/24 15:15:50 by  rleite-s        ###   ########.fr       */
+/*   Updated: 2023/06/25 04:56:43 by  rleite-s        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,26 +39,24 @@ void	ft_adjust_delimiters(char *s)
 	char	*end;
 
 	end = s;
-	while (ft_isspace(*end))
+	while (end && ft_isspace(*end))
 		end++;
-	if (end != s)
+	if (end && end != s)
 		ft_memmove(s, end, ft_strlen(end) + 1);
-	while (*s)
+	while (s && *s)
 	{
 		if (*s == '\'' || *s == '"')
 			s = ft_strchr(s + 1, *s);
-		if (ft_isspace(*s))
+		if (s && ft_isspace(*s))
 		{
 			end = s + 1;
 			while (ft_isspace(*end))
 				end++;
-			if (!*end)
-				*s = 0;
-			else
-				*s = ' ';
+			*s = ' ' * (*end != '\0');
 			if (s + 1 != end)
 				ft_memmove(s + 1, end, ft_strlen(end) + 1);
 		}
-		s++;
+		if (s)
+			s++;
 	}
 }
